3610d25fd468235a1188df591b919d36dd33eb86
Rubait Reshad <rubaitreshad@gmail.com>
2026-07-16 09:38:14 +0600

    158A accepted w 92ms

--- 158A_next_round.cpp
+++ 158A_next_round.cpp
@@ -4,13 +4,23 @@ using namespace std;
 
 int main (){
     short n,k, input, count {};
+
+    cin >> n >> k;
+
+    int arr[n];
     
-    for (short i=0; i<n; i++){
-        cin >> input;
-        if (input>k){
+    for (int i=0; i<n; i++){
+
+        cin >> arr[i];
+    }
+    
+        for (int i=0; i<n; i++){
+            if (arr[i] >= arr[k-1] && arr[i]>0){
             count++;
         }
     }
+
+
 
     cout << count;
 }
